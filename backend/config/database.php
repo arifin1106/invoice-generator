07d5cf6c1eba409656be30c69c890cc92751e446
arifin1106 <arfnafin11@gmail.com>
2026-09-14 11:52:30 +0700

    Fix: PDO emulate prepares for Supabase PgBouncer

--- backend/config/database.php
+++ backend/config/database.php
@@ -87,6 +87,9 @@ return [
         'pgsql' => [
             'driver' => 'pgsql',
             'url' => env('DB_URL'),
+            'options' => [
+                PDO::ATTR_EMULATE_PREPARES => true,
+            ],
             'host' => env('DB_HOST', '127.0.0.1'),
             'port' => env('DB_PORT', '5432'),
             'database' => env('DB_DATABASE', 'laravel'),
